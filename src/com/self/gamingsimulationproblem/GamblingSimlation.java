c252fc6aa8334de4df84323283e46b44b2bf1a00
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-09 14:14:57 +0530

    [ADDED]:Husain - Setting up stack and bet

---
+++ src/com/self/gamingsimulationproblem/GamblingSimlation.java
@@ -0,0 +1,15 @@
+package com.self.gamingsimulationproblem;
+
+public class GamblingSimlation {
+
+
+		private static final int STAKE_PER_DAY = 100;
+		private static final int BET_PER_GAME = 1;
+
+		public static void main(String[] args) {
+			System.out.println("The Stake per day is: " +STAKE_PER_DAY);
+			System.out.println("The Bet per game is: " +BET_PER_GAME);
+		}
+}
+
+
