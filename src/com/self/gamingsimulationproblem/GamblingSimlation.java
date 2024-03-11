0cb146bd175189a7ea57b35050d9bd4ccb1c4ea6
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-11 12:39:28 +0530

    [MODIFIED]:Husain - Game for day

--- src/com/self/gamingsimulationproblem/GamblingSimlation.java
+++ src/com/self/gamingsimulationproblem/GamblingSimlation.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class GamblingSimlation {
 
 
-		private static final int STAKE_PER_DAY = 100;
+		private static final int STACK_PER_DAY = 100;
 		private static final int BET_PER_GAME = 1;
 		private static final int WIN = 1;
 		private static final int LOOSE = 0;
@@ -13,28 +13,37 @@ public class GamblingSimlation {
 		private static int looseStack = 0;
 		
 		public static void gameStatus() {
-			System.out.println("The Stake per day is: " +STAKE_PER_DAY);
+			System.out.println("The Stake per day is: " +STACK_PER_DAY);
 			System.out.println("The Bet per game is: " +BET_PER_GAME);
 			System.out.println("Wining Stack"+winStack);
 			System.out.println("Loosint Stack"+looseStack);
-			System.out.println("**************************");
+			System.out.println("----------------------------");
 		}
 		
 		public static void gamePlay() {
-			Random random = new Random();
-			int playStatus = random.nextInt(9)%2;
-			if(playStatus == LOOSE) {
-				looseStack += 1;
-			}else {
-				winStack += 1;
+			
+			while(true) {
+				gameStatus();
+				Random random = new Random();
+				int playStatus = random.nextInt(9)%2;
+				if(playStatus == LOOSE) {
+					looseStack += 1;
+				}else {
+					winStack += 1;
+				}
+				if(winStack == STACK_PER_DAY / 2) {
+					break;
+				}
+				if(looseStack == STACK_PER_DAY / 2) {
+					break;
+				}
 			}
 			
 		}
 		public static void main(String[] args) {
 			System.out.println("Welcome to Gambling Simulation Problem developed by Mohammad Husain");
-			gameStatus();
 			gamePlay();
-			gameStatus();
+			
 
 		}
 }
