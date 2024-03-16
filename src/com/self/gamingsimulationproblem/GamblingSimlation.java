87b8dc1165ef30be3a0d2d550f5a83596f20d155
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-16 13:31:54 +0530

    [MODIFIED]:Husain - Calculated Luckiest and Unluckiest day

--- src/com/self/gamingsimulationproblem/GamblingSimlation.java
+++ src/com/self/gamingsimulationproblem/GamblingSimlation.java
@@ -16,7 +16,8 @@ public class GamblingSimlation {
 		
 		private static int winDays = 0;
 		private static int lostDays = 0;
-		
+		private static int looseStatus[] = new int [  MONTHLY_STACK ];
+		private static int winStatus[] = new int [  MONTHLY_STACK ];
 		public static void gameStatus() {
 			System.out.println("The Stake per day is: " +STACK_PER_DAY);
 			System.out.println("The Bet per game is: " +BET_PER_GAME);
@@ -37,7 +38,7 @@ public class GamblingSimlation {
 				
 				Random random = new Random();
 				int playStatus = random.nextInt(9)%2;
-				if(playStatus != WIN) {
+				if(playStatus == LOOSE) {
 					looseStack += 1;
 				}else {
 					winStack += 1;
@@ -56,16 +57,34 @@ public class GamblingSimlation {
 			loosingAmount_PerDay[i] = looseStack;
 			
 			
-	//		System.out.println("Winnig Stack on Day"+(i+1)+" :  " +winStack);
-	//		System.out.println("Loosing Stack on Day "+(i+1)+" : " +looseStack);
+			//System.out.println("Winnig Stack on Day"+(i+1)+" :  " +winStack);
+			//System.out.println("Loosing Stack on Day "+(i+1)+" : " +looseStack);
 			System.out.println("Winning Amount"+winningAmount_PerDay[i]);
 			System.out.println("Loosing Amount"+loosingAmount_PerDay[i]);
+			
 			
 			System.out.println("------------------------");
 			
 			}
-				System.out.println("Wining days in month"+winDays);
-				System.out.println("Loosing days in month"+lostDays);
+				System.out.println("Wining days in month "+winDays);
+				System.out.println("Loosing days in month "+lostDays);
+				
+				int luckiestDay = winStatus[0];
+				int unluckiestDay = looseStatus[0];
+				
+				
+				for(int i = 1; i < MONTHLY_STACK; i++) {
+					winStatus[i] = winningAmount_PerDay[i] - loosingAmount_PerDay[i];
+					if(winStatus[i] > luckiestDay) {
+						luckiestDay = winStatus[i];
+					}
+					looseStatus[i] = loosingAmount_PerDay[i] - winningAmount_PerDay[i];
+					if(looseStatus[i] > unluckiestDay) {
+						unluckiestDay = looseStatus[i];
+					}
+				}
+				System.out.println("Luckiest Day = "+luckiestDay);
+				System.out.println("Unlckiest Day = "+unluckiestDay);
 		}
 		public static void main(String[] args) {
 			System.out.println("Welcome to Gambling Simulation Problem developed by Mohammad Husain");
